48ea089e158ca569c243e7fe329a4bf06cad2266
Пырцак Герман FO1ULI <rbebi96@mail.ru>
2025-05-07 23:18:43 +0700

    Создал окно авторизации

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,9 +1,10 @@
-
+import AuthorizationPage from "./pageElement/AuthorizationPage";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <AuthorizationPage/>
 
     </div>
   );
