7c16c6caac8b624af889be14f59444482b7b9fd0
Пырцак Герман FO1ULI <rbebi96@mail.ru>
2025-05-08 22:48:23 +0700

    Добавление Formik и yup для авторизации

--- final-project/src/components/pageElement/AuthorizationPage.jsx
+++ final-project/src/components/pageElement/AuthorizationPage.jsx
@@ -1,34 +1,74 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import styles from '../../styles/AuthorizationPage.module.css';
 
 const AuthorizationPage = () => {
+    const validationSchema = Yup.object().shape({
+        username: Yup.string()
+            .required('Обязательное поле')
+            .min(3, 'Логин должен содержать минимум 3 символа'),
+        password: Yup.string()
+            .required('Обязательное поле')
+            .min(6, 'Пароль должен содержать минимум 6 символов'),
+    });
+    // ЗДЕСЬ БУДЕТ API ДЛЯ АВТОРИЗАЦИИ В БД!!!!
+    const handleSubmit = (values, { setSubmitting }) => {
+        console.log('Отправка данных:', values);
+        setTimeout(() => {
+            setSubmitting(false);
+        }, 1000);
+    };
+
     return (
         <div className={styles.pageContainer}>
             <div className={styles.backgroundAnimation}></div>
             <div className={styles.window__authorization}>
                 <div className={styles.auth__container}>
                     <h1 className={styles.auth__title}>Авторизация</h1>
-                    <form className={styles.auth__form}>
-                        <div className={styles.form__group}>
-                            <label htmlFor="username" className={styles.form__label}>Логин</label>
-                            <input
-                                type="text"
-                                id="username"
-                                className={styles.form__input}
-                                placeholder="Введите ваш логин"
-                            />
-                        </div>
-                        <div className={styles.form__group}>
-                            <label htmlFor="password" className={styles.form__label}>Пароль</label>
-                            <input
-                                type="password"
-                                id="password"
-                                className={styles.form__input}
-                                placeholder="Введите ваш пароль"
-                            />
-                        </div>
-                        <button type="submit" className={styles.auth__button}>Войти</button>
-                    </form>
+
+                    <Formik
+                        initialValues={{ username: '', password: '' }}
+                        validationSchema={validationSchema}
+                        onSubmit={handleSubmit}
+                    >
+                        {({ isSubmitting }) => (
+                            <Form className={styles.auth__form}>
+                                <div className={styles.form__group}>
+                                    <label htmlFor="username" className={styles.form__label}>Логин</label>
+                                    <Field
+                                        type="text"
+                                        name="username"
+                                        id="username"
+                                        className={styles.form__input}
+                                        placeholder="Введите ваш логин"
+                                    />
+                                    <ErrorMessage name="username" component="div" className={styles.error__message} />
+                                </div>
+
+                                <div className={styles.form__group}>
+                                    <label htmlFor="password" className={styles.form__label}>Пароль</label>
+                                    <Field
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        className={styles.form__input}
+                                        placeholder="Введите ваш пароль"
+                                    />
+                                    <ErrorMessage name="password" component="div" className={styles.error__message} />
+                                </div>
+
+                                <button
+                                    type="submit"
+                                    className={styles.auth__button}
+                                    disabled={isSubmitting}
+                                >
+                                    {isSubmitting ? 'Вход...' : 'Войти'}
+                                </button>
+                            </Form>
+                        )}
+                    </Formik>
+
                     <div className={styles.auth__links}>
                         <a href="#" className={styles.auth__link}>Забыли пароль?</a>
                         <a href="#" className={styles.auth__link}>Регистрация</a>
