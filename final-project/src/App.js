68e4cbdd491eedd3c2915f92405ddce33f12cdaf
Пырцак Герман FO1ULI <rbebi96@mail.ru>
2025-05-23 20:26:18 +0700

    Добавил страницу регистрации

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import MainProfile from './components/MainProfile/RouterConfig.jsx';
 import './App.css';
 import AuthorizationPage from "./components/pageElement/AuthorizationPage";
+import RegPage from "./components/pageElement/RegPage";
 
 function App() {
     return (
@@ -10,7 +11,8 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Navigate to="/login" replace />} />
                     <Route path="/login" element={<AuthorizationPage/>} />
-                    <Route path="/*" element={<MainProfile/>} />
+                    <Route path="/register" element={<RegPage />} />
+                    <Route path="/" element={<MainProfile/>} />
                 </Routes>
             </div>
         </Router>
