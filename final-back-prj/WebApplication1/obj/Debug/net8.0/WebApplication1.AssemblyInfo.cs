ac47b82190fa1ab03ab6c75efea04bfef7ce047f
ip316s26 <nicksh.04@mail.ru>
2025-05-24 22:50:52 +0700

    гитигнор расширил

--- final-back-prj/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ final-back-prj/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b57c4bdfa0b57c850b50012434bba0b260a3eccf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325675313bb88978929a279e3ad2d23290b691c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
